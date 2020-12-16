5498ed2d81a560f054d3c90d6bdcbc8f908992fb
deadman00069 <konarkshivam1997@gmail.com>
2020-12-16 22:01:50 +0530

    init

--- bmicalci/android/app/src/main/kotlin/com/example/bmicalci/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bmicalci
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- todoe/lib/model/task.dart
+++
@@ -1,9 +0,0 @@
-class Task
-{
-  final String title;
-  bool isDone;
-  Task({this.title,this.isDone=false});
-  toggleDone(){
-    isDone=!isDone;
-  }
-}
